8478bea2a9edcf3763e02162ef892a7a0bd4457d
Abhinav-2101 <abhinavkumarv2101@gmail.com>
2024-09-10 21:17:50 +0530

    valentine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import loveGif from './lovegif.gif'; 
 
 function App() {
+  const [yesButtonSize, setYesButtonSize] = useState(16); 
+  const [noButtonText, setNoButtonText] = useState("No");
+  const [noClickCount, setNoClickCount] = useState(0);
+  const [showContent, setShowContent] = useState(true);
+
+  const noResponses = [
+    "Serious ah va", "Poda pota, please", "enaku vera yaaru irukhanga", "Ego ahhh", 
+    "Please purinjiko", "Dei unaku vera enna vazhi", "Enna mela cheat panra", "Adingu", 
+    "Un veetuku vandhuduven"
+  ];
+
+  const handleNoClick = () => {
+    setYesButtonSize(yesButtonSize + 20); 
+    if (noClickCount < 9) {
+      setNoButtonText(noResponses[noClickCount]);
+      setNoClickCount(noClickCount + 1);
+    } else {
+      setNoButtonText("Seriously, no?");
+    }
+  };
+
+  const handleYesClick = () => {
+    setShowContent(false); 
+  };
+
+  if (!showContent) {
+    return <div className="centered">
+      <div>
+      <img src={loveGif} alt="lovegif" />
+      </div>
+      <div>
+       <p>AWW BABY! YAY!</p>
+       </div>
+    </div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Will you be my Valentine?</p>
+        <div style={{ display: 'inline-block'}}>
+          <button className="yes-button" style={{ fontSize: `${yesButtonSize}px` }} onClick={handleYesClick}>Yes</button>
+          <button className="no-button" onClick={handleNoClick}>{noButtonText}</button>
+        </div>
       </header>
-    </div>
+  </div>
   );
 }
 
